8c5ac4ca36970ed04c365210f4c2412cdc1cf9b2
MrDj200 <mrdj201@gmail.com>
2026-03-31 23:01:20 +0200

    Added refresh button

---
+++ MuddyStargateWasm/AppBarState.cs
@@ -0,0 +1,26 @@
+﻿namespace MuddyStargateWasm
+{
+    public class AppBarState
+    {
+        public bool ShowRefreshButton { get; private set; }
+        public Action? OnRefreshClicked { get; private set; }
+
+        public event Action? OnChange;
+
+        public void SetRefresh(Action onClick)
+        {
+            ShowRefreshButton = true;
+            OnRefreshClicked = onClick;
+            NotifyStateChanged();
+        }
+
+        public void ClearRefresh()
+        {
+            ShowRefreshButton = false;
+            OnRefreshClicked = null;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
